a886d923a0e6be47c9a9f639a7ed0a31b8b6090e
Ojaswi <ojaswiawasthi2025@gmail.com>
2021-03-16 19:56:51 +0530

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {Switch,Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      Hi
-    </div>
+    <Switch>
+      <Route exact path="/">
+        This is the home page
+      </Route>
+
+      <Route exact path="/starred">
+        This is the starred page
+      </Route>
+      
+      <Route>
+        This is 404 page
+      </Route>
+    </Switch>
   );
 }
 
