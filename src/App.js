3e2adb492f37a6c82df82f5ac275b84f35c224e6
Ojaswi <ojaswiawasthi2025@gmail.com>
2021-03-17 02:20:20 +0530

    added the title and navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import {Switch,Route } from 'react-router-dom';
-import Navs from './components/Navs'
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 
 function App() {
   return (
-    <div>
-      <Navs />
       
       <Switch>
         <Route exact path="/">
@@ -22,7 +19,7 @@ function App() {
           This is 404 page
         </Route>
       </Switch>
-    </div>
+    
   );
 }
 
