ebc726e349c598417455544621ae87c0f45670f0
Ojaswi <ojaswiawasthi2025@gmail.com>
2021-03-17 19:47:24 +0530

    successfully fetched from the api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,13 @@ import MainPageLayout from '../components/MainPageLayout'
 const Home = () => {
 
     const [input,setInput]=useState('');
+    const [results,setResults]=useState(null);
 
     const onSearch= () => {
-        fetch(`https://http://api.tvmaze.com/search/shows?q=${input}`)
+        fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
         .then(r=> r.json())
         .then(result => {
+            setResults(result);
             console.log(result);
         })
     };
@@ -23,11 +25,31 @@ const Home = () => {
         }
     }
 
+    const renderResults =() => {
+        if (results && results.length === 0)
+        {
+            return <div>No Results</div>
+        }
+        if (results && results.length > 0)
+        {
+            return (
+            <div>
+                {results.map(item => (
+                    <div key={item.show.id}> {item.show.name}</div>
+                ))}
+            </div>
+            );
+        }
+        return null;
+
+    }
+
     
     return (
         <MainPageLayout>
         <input type="text" onChange={onInputChange} onKeyDown={onKeyDown} value={input}/>
         <button type="button" onClick={onSearch}>Search</button>
+        {renderResults()}
         </MainPageLayout>
     )
 }
