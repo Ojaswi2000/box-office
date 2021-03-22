518d939b8b06bbc70728ccdde513ed23f3107674
Ojaswi <ojaswiawasthi2025@gmail.com>
2021-03-23 03:15:07 +0530

    added 2 more custom hooks-useShow and useLastQuery

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,4 +1,5 @@
-import {useReducer,useEffect} from 'react'
+import {useReducer,useEffect,useState} from 'react'
+import {apiGet} from '../misc/config';
 
 function showsReducer(prevState,action){
     switch(action.type)
@@ -31,4 +32,69 @@ function usePersistedReducer(reducer,initialState,key) {
 }
     export function useShows(key="shows"){
         return usePersistedReducer(showsReducer, [], key);
+    }
+
+
+    export function useLastQuery(key="lastQuery"){
+        const [input,setInput]=useState(()=>{
+            const persisted = sessionStorage.getItem(key);
+
+            return persisted ? JSON.parse(persisted) : '';
+        });
+
+        const setPersistedInput= (newState)=>{
+            setInput(newState);
+            sessionStorage.setItem(key,JSON.stringify(newState));
+        }
+        return [input,setPersistedInput];
+    }
+
+
+    const reducer = (prevState, action) => {
+        switch(action.type) {
+    
+            case 'FETCH_SUCCESS' :{
+                return {isLoading: false, errors : null, show: action.show};
+            }
+    
+            case 'FETCH_FAILED' :{
+                return {...prevState, isLoading : false, errors: action.errors}
+            }
+            default : 
+            return prevState;
+        }
+    };
+
+
+
+    export function useShow(showID){
+        const [ state, dispatch]= useReducer(reducer,
+            {
+            show:null,
+            isLoading: true,
+            errors: null
+            }
+            );
+        
+         useEffect(()=>{
+     
+             let isMounted = true;
+             apiGet(`/shows/${showID}?embed[]=seasons&embed[]=cast`)
+             .then(results => {
+                 if(isMounted) {
+                     dispatch({type: 'FETCH_SUCCESS', show : results });
+                 }
+             })
+             .catch(err => {
+                 if(isMounted) {
+                     dispatch ({type: 'FETCH_FAILED', errors : err.message});
+                 }
+             });
+     
+             return ()=>{
+                 isMounted=false;
+             }
+         },[showID]);
+     
+         return state;
     }
